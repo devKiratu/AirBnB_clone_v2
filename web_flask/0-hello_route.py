2395c465ec809b6a98ad3a919d2372534d062056
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-22 20:50:20 +0300

    add 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application listening on 0.0.0.0:5000
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """displays 'hello HBNB!' on route /"""
+    return "Hello HBNB!\n"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
