65c0479c6cea50d9f1cd2c5e739c6941a95608ee
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-09-27 17:07:21 +0300

    edit module documentation to remove colons

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script starts a Flask web app at 0.0.0.0:5000"""
+"""This script starts a Flask web app"""
 
 from flask import Flask, render_template
 from models import storage
